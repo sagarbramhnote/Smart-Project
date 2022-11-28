44f2c6bc39e647c83f59e9a3e139358156a28b7c
RamaPayyala <ramakrishna.payyala@gmail.com>
2022-11-28 12:17:30 +0530

    store update working

--- src/app/store/updatestore/updatestore.component.ts
+++ src/app/store/updatestore/updatestore.component.ts
@@ -7,7 +7,8 @@ import { environment } from 'environments/environment';
 @Component({
   selector: 'app-updatestore',
   templateUrl: './updatestore.component.html',
-  styleUrls: ['./updatestore.component.scss']
+  styleUrls: ['./updatestore.component.scss'],
+  providers: [NGXToastrService]
 })
 export class UpdatestoreComponent implements OnInit {
 
@@ -27,16 +28,16 @@ export class UpdatestoreComponent implements OnInit {
 
   ngOnInit() {
     this.storeInfoRequest = JSON.parse(localStorage.getItem('editStore'));
-    let a= (localStorage.getItem('id'))
-    console.log('this is number ' + a)
-    console.log(this.storeInfoRequest.id)
+    // let a= (localStorage.getItem('id'))
+    // console.log('this is number ' + a)
+    // console.log(this.storeInfoRequest.id)
 
-    return this.http.get<StoreInfoRequest>(environment.smartSafeAPIUrl + "/storeinfo/" + a,this.httpOptions).subscribe(data =>{
-      console.log(data)
+    // return this.http.get<StoreInfoRequest>(environment.smartSafeAPIUrl + "/storeinfo/" + a,this.httpOptions).subscribe(data =>{
+    //   console.log(data)
   
-      this.storeInfoRequest = data
-      console.log('print the store ' + this.storeInfoRequest)
-     })
+    //   this.storeInfoRequest = data
+    //   console.log('print the store ' + this.storeInfoRequest)
+    //  })
   }
 
   getStoreList() {
