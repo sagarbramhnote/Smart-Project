5ddf1ef04227df2353ac1858f303b7549bdad254
ShaliniEdara <edara.shalini@gmail.com>
2022-12-27 16:16:42 +0530

    role-management-validation

--- src/app/role/createrole/createrole.component.ts
+++ src/app/role/createrole/createrole.component.ts
@@ -74,7 +74,8 @@ export class CreateroleComponent implements OnInit {
         } else {
           console.log("Server-side error occured.");
         }
-        this.service.typeWarning();
+        //this.service.typeWarning();
+        this.service.typeCustommessage(err.error.message);
       });
     console.log(JSON.stringify(this.role));
     this.getAllRolesList();
