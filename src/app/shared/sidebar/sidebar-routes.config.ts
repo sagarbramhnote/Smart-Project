097234d4b5e5993a18bd8f472bac5eb167fb3949
admin1 <sagarbramhnote@gmail.com>
2023-02-20 16:41:47 +0530

    update

--- src/app/shared/sidebar/sidebar-routes.config.ts
+++ src/app/shared/sidebar/sidebar-routes.config.ts
@@ -6,20 +6,20 @@ export const ROUTES: RouteInfo[] = [
     { path: '/role/createrole', title: 'Role Management ', icon: 'ft-star', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
     { path: '/store/createstore', title: 'Store Management ', icon: 'ft-shopping-cart', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
     { path: '/user-management', title: 'User Management ', icon: 'ft-user', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-   //  { path: '', title: 'kiosk Management ', icon: 'ft-user', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false, 
-   //       submenu: [
-   //          { path: '/kiosk/newkiosk', title: 'Add Kiosk', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-   //          { path: '/kiosk/bill-validator', title: 'Add Bill Validator', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-   //          { path: '/kiosk/printer', title: 'Add Printer', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-   //          { path: '/kiosk/locks', title: 'Add Locks', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+    { path: '', title: 'kiosk Management ', icon: 'ft-user', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false, 
+         submenu: [
+            { path: '/kiosk/newkiosk', title: 'Add Kiosk', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+            { path: '/kiosk/bill-validator', title: 'Add Bill Validator', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+            { path: '/kiosk/printer', title: 'Add Printer', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+            { path: '/kiosk/locks', title: 'Add Locks', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
 
-   //       ] 
-   //  },
+         ] 
+    },
     { path: '', title: 'Assign To Store ', icon: 'ft-shopping-cart', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false, 
     submenu: [
-   //    { path: '/assign/assignstore', title: 'Assign Store', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+       { path: '/assign/assignstore', title: 'Assign Store', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
        { path: '/assign/assign-user-to-store', title: 'Assign User To Store', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-   //    { path: '/assign/assign-permission', title: 'Assign Permissions', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+       { path: '/assign/assign-permission', title: 'Assign Permissions', icon: ''  , class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
 
     ] 
     },
